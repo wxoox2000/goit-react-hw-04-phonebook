c6ccd436fbb993c725bd4475e4a978764039f769
wxoox2000 <wxoox2000@gmail.com>
2023-09-10 19:57:39 +0300

    refactor#2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,24 +5,15 @@ import { ContactList } from './ContactList/ContactList';
 import { nanoid } from 'nanoid';
 import { Wrap, Heading } from './App.styled';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useMemo } from 'react';
 
 export const App = () => {
-  // state = {
-  //   contacts: [
-  //     { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  //     { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  //     { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  //     { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  //   ],
-  //   filter: '',
-  // };
-  const baseContacts = [
+  const baseContacts = useMemo(() => [
     { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
     { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
     { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
     { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ];
+  ], []);
   const [contacts, setContacts] = useState(baseContacts);
   const [filter, setfilter] = useState('');
   useEffect(() => {
